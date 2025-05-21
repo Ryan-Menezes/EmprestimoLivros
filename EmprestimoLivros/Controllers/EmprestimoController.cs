0b32c87c0b117269f07b5dabe8450aff602c8974
Ryan de Menezes <ryan.menezes@digitalindustry.com.br>
2025-05-21 15:31:46 -0300

    first commit

---
+++ EmprestimoLivros/Controllers/EmprestimoController.cs
@@ -0,0 +1,83 @@
+﻿using EmprestimoLivros.Data;
+using EmprestimoLivros.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmprestimoLivros.Controllers
+{
+    public class EmprestimoController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public EmprestimoController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var emprestimos = _db.Emprestimos;
+
+            return View(emprestimos);
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar([FromForm] Emprestimo emprestimo)
+        {
+            if (!ModelState.IsValid) return View();
+
+            _db.Emprestimos.Add(emprestimo);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Editar(int? id)
+        {
+            if (id == null || id == 0) return NotFound();
+
+            var emprestimo = _db.Emprestimos.FirstOrDefault(x => x.Id == id);
+
+            if (emprestimo == null) return NotFound();
+
+            return View(emprestimo);
+        }
+
+        [HttpPost]
+        public IActionResult Editar([FromForm] Emprestimo emprestimo)
+        {
+            if (!ModelState.IsValid) return View();
+
+            _db.Emprestimos.Update(emprestimo);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Excluir(int? id)
+        {
+            if (id == null || id == 0) return NotFound();
+
+            var emprestimo = _db.Emprestimos.FirstOrDefault(x => x.Id == id);
+
+            if (emprestimo == null) return NotFound();
+
+            return View(emprestimo);
+        }
+
+        [HttpPost]
+        public IActionResult Excluir([FromForm] Emprestimo emprestimo)
+        {
+            if (emprestimo == null) return NotFound();
+
+            _db.Emprestimos.Remove(emprestimo);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ EmprestimoLivros/Models/Emprestimo.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmprestimoLivros.Models
+{
+    public class Emprestimo
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Esse campo é obrigatório")]
+        public string Recebedor { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Esse campo é obrigatório")]
+        public string Fornecedor { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Esse campo é obrigatório")]
+        public string LivroEmprestado {  get; set; } = string.Empty;
+
+        public DateTime DataAtualizacao { get; set; } = DateTime.Now;
+    }
+}
